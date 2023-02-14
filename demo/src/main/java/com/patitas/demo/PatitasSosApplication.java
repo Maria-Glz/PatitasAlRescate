6db4986a985a9e3b274d20ea3657bb2bc0a00f20
maria <maria.gonzalezbarrreda@.com>
2023-02-13 19:10:44 -0600

    Se agrego la carpeta demo

---
+++ demo/src/main/java/com/patitas/demo/PatitasSosApplication.java
@@ -0,0 +1,13 @@
+package com.patitas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitasSosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitasSosApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/patitas/demo/PatitasSosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patitas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitasSosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
